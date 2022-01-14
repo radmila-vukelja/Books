e8d9f9bcaa61da54ce6076f43b2d5589d971e01f
radmila-vukelja <emailzaseminarskitest123321>
2022-01-14 20:49:39 +0100

    DB-store+fetch

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Book = require('./models/book');
 
 const app = express();
 
+
+mongoose.connect("mongodb+srv://admin:admin@cluster0.tiv7z.mongodb.net/Library?retryWrites=true&w=majority")
+.then(() => {
+    console.log('Connected to database');
+})
+.catch(() => {
+    console.log('Not connected');
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, OPTIONS, DELETE');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, OPTIONS, DELETE");
     next();
 });
 
-app.use("/api/books", (req, res, next) => {
-    const books = [
-        { id: 'sefae', title: 'back title', author: 'back author', description: 'back desc' },
-        { id: 'dj4923q0dfj[', title: 'back title2', author: 'b2ack author', description: 'back2 desc' },
-    ]
-    res.status(200).json({
-        message: 'Books fetched successfully',
-        books: books
+app.post("/api/books", (req, res, next) => {
+    const book = new Book({
+        title: req.body.title,
+        author: req.body.author,
+        description: req.body.description
     });
+    book.save();
+    res.status(201).json({
+        message: 'Book created. All OK'
+    })
+});
+
+app.get("/api/books", (req, res, next) => {
+    Book.find()
+    .then(documents => {
+        res.status(200).json({
+            message: 'Books fetched successfully',
+            books: documents
+        });
+    });
+    
 });
 
 
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { Book } from '../models/book';
 
@@ -17,9 +18,20 @@ export class BooksService {
   private booksUpdated = new Subject<Book[]>();
 
   getBooks(){
-    this.http.get<{message: string, books: Book[]}>("http://localhost:3000/api/books")
-    .subscribe((bookInfo) => {
-      this.books = bookInfo.books;
+    this.http.get<{message: string, books: any}>("http://localhost:3000/api/books")
+    .pipe(map((bookInfo) => {
+      return bookInfo.books.map((book: { title: any; author: any; description: any; _id: any; }) => {
+        return {
+          title: book.title,
+          author: book.author,
+          description: book.description,
+          id: book._id
+        } //pipe map shit = transforming data from DB so
+          // the id would match _id
+      });
+    }))
+    .subscribe((transformedBooks) => {
+      this.books = transformedBooks;
       this.booksUpdated.next([...this.books]);
     });
   }
@@ -30,7 +42,11 @@ export class BooksService {
 
   addBook(title: string, author: string, description: string) {
     const book: Book = { id: null, title: title, author: author, description: description };
-    this.books.push(book);
-    this.booksUpdated.next([...this.books]);
+    this.http.post<{message: string}>("http://localhost:3000/api/books", book)
+    .subscribe((responseData) => {
+      console.log(responseData.message);
+      this.books.push(book);
+      this.booksUpdated.next([...this.books]);
+    });
   }
 }
